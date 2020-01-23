5130c1a8c1f089dc349253ed2560031a8fd95cc6
Dafferianto Trinugroho <daft@equinor.com>
2020-01-23 08:23:08 +0100

    List contents of a submodule directory

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+const directory = "./alpine-git-lfs"
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -12,5 +15,15 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "The path you entered: %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "The path you entered: %s \n", r.URL.Path[1:])
+
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		fmt.Fprintf(w, "Could not read submodule directory: %v", err)
+	}
+
+	fmt.Fprintf(w, "Contents of %s submodule directory: \n", directory)
+	for _, file := range files {
+		fmt.Fprintln(w, file.Name())
+	}
 }
